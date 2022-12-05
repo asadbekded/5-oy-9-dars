9dcc0b031e6affb4bb32f2f787ff354638884374
asadbekded <asadbek171@gmail.com>
2022-12-05 23:27:20 +0500

    Add new change

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -31,7 +31,7 @@ export const Modal = ({ modal, setModal, children }) => {
     <div ref={overlayRef} onClick={handleCloseModal} className="overlay">
       <div className="bg-white w-50 p-5 rounded-10 just">
          <div className="modal-header">
-            <h3>Title</h3>
+            <h3>Add</h3>
             <button onClick={() => setModal(false)} className="btn btn-danger">&times;</button>
          </div>
          <div className="modal-content">
--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -1,24 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import  axios  from 'axios';
 import { Modal } from "../../components/Modal/Modal";
+import { Input } from "../../components/Input/Input";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 
 export const Posts = () => {
-   const [ modal, setModal ] = useState(false);
 
+   const { me } = useContext(UserContext);
+
+   const titleRef = useRef();
+   const bodyRef = useRef();
+
+   const [ modal, setModal ] = useState(false);
    const [ posts, setPosts ] = useState({});
+
 
    useEffect(() =>{
       axios.get("http://localhost:8080/posts")
-      .then(res => console.log(res))
+      .then(res => setPosts(res.data))
       .catch(err => console.log(err))
    }, [])
+
+   const handleAddPost = (evt) => {
+      evt.preventDefault()
+
+      axios.post("http://localhost:8080/posts", {
+         userId: me.id,
+         title: titleRef.current.value,
+         body: bodyRef.current.value,
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+   }
 
   return (
     <div className="container">
       <h3 className="text-center my-3">Posts page</h3>
       <button onClick={() => setModal(true)} className="btn btn-primary">+ Create post</button>
 
-      {modal && <Modal modal={modal} setModal={setModal} />}
+      {
+         posts.length ? <ul className="p-0 m-0 mt-4">
+         {
+            posts.map((el) =>(
+            <li key={el.title} className="d-flex align-items-center w-50 shadow p-3 mb-2">
+               <h4>{el.title}</h4>
+               <h5 className="ms-4">{el.body}</h5>
+            </li>))
+         }
+         </ul>: <h2>Loading...</h2>
+      }
+
+      {modal && <Modal modal={modal} setModal={setModal} >
+         <form onSubmit={handleAddPost} className="mt-4">
+            <Input ref={titleRef} type="text" placeholder="Title"/>
+            <Input ref={bodyRef} type="text" placeholder="Body"/>
+            <button className="btn btn-primary">POST</button>
+         </form>
+      </Modal>}
     </div>
   )
 }
